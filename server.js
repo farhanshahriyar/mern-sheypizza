e3cd5c086d9bbc097a9eeebaacae99d9c1e782c5
farhanshahriyar <farhan.abir9999@gmail.com>
2022-02-27 22:38:30 +0600

    imported today

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 const express = require("express");
 const req = require("express/lib/request");
 
+const Pizza = require('./client/models/pizzaModel');
 const app = express();
+const db = require("./db")
 
 app.use(express.json());
 
 
 
 app.get("/", (req , res) => {
-    res.send("Server is working! :) ");
+    res.send("Server is working! :) 🔥 ");
 });
 
+app.get("/getpizzas",(req, res) => {
+
+    Pizza.find({} , (err , docs) => {
+
+        if(err) {
+            console.log(err);
+        }
+        else{
+            res.send(docs);
+        }
+    })
+
+});
 const port = process.env.PORT || 5000; 
 
 app.listen(port, () => `Server running on port :( )`);
