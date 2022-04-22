09832021ca20aa2b640e98564dc576f61631dc8b
farhanshahriyar <farhan.abir9999@gmail.com>
2022-04-22 19:26:02 +0600

    added filtering function

--- client/src/actions/pizzaActions.js
+++ client/src/actions/pizzaActions.js
@@ -10,3 +10,25 @@ export const getAllPizzas=()=>async dispatch=>{
         dispatch({type:'GET_PIZZAS_FAILED' , payload : error})
     }
 }
+
+//filter pizza from filter.js
+export const filterPizzas=(searchkey , category)=>async dispatch=>{
+
+    var filteredPizzas ;
+    dispatch({type:'GET_PIZZAS_REQUEST'})
+
+    try {
+         const response = await axios.get('/api/pizzas/getallpizzas')
+         filteredPizzas = response.data.filter(pizza=>pizza.name.toLowerCase().includes(searchkey)) //searching process
+        
+         if (category!='all')
+         {
+            filteredPizzas = response.data.filter(pizza=>pizza.category.toLowerCase()==category) //category process 
+         }
+        
+        
+         dispatch({type:'GET_PIZZAS_SUCCESS' , payload : filteredPizzas})
+    } catch (error) {
+        dispatch({type:'GET_PIZZAS_FAILED' , payload : error})
+    }
+}
